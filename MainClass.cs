ecf5da0be13548e630a64f58af110164d56e06bb
ZeroRL <zerorl@qq.com>
2025-05-23 22:54:38 +0800

    Fixed

--- MainClass.cs
+++ MainClass.cs
@@ -1,56 +1,44 @@
 ﻿using HarmonyLib;
+using LabApi.Events.Arguments.Scp914Events;
 using LabApi.Events.CustomHandlers;
 using LabApi.Events.Handlers;
 using LabApi.Features.Console;
 using LabApi.Loader.Features.Plugins;
+using Scp914;
 using SCPSLBugPatch.Patches;
 using System;
 using System.IO;
 
 namespace SCPSLBugPatch
 {
-    internal class MainClass :Plugin
+    internal class MainClass : Plugin
     {
         private const string PluginName = "SCPSLBugPatch";
         private static string LogFilePath { get; set; }
-        private static Harmony Harmony { get; } = new Harmony($"{PluginName}");
-
+        private static Harmony Harmony { get; } = new Harmony(PluginName);
         public override string Name => PluginName;
-
         public override string Description => null;
-
         public override string Author => "ZeroRL";
-
-        public override Version Version => new Version(1,0,0,0);
-
-        public override Version RequiredApiVersion => new Version(1,0,2,0);
+        public override Version Version => new Version(1, 1, 0, 0);
+        public override Version RequiredApiVersion => LabApi.Features.LabApiProperties.CurrentVersion;
         internal static void AddLog(string content)
         {
             Logger.Info(content);
-            if (LogFilePath != null)
-            {
-                File.AppendAllText(LogFilePath, $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}] [{ServerStatic.ServerPort}] {content}\r\n");
-            }
+            File.AppendAllText(LogFilePath, $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}] [{ServerStatic.ServerPort}] {content}\r\n");
         }
         private void OnRoundRestart()
         {
             OnMessageReceivedPatch.LogBadDataInfo();
             OnMessageReceivedPatch.Initialize();
         }
-
         public override void Enable()
         {
-
             string folder = FileManager.GetAppFolder();
-            if (Directory.Exists(folder))
-            {
-                LogFilePath = Path.Combine(folder, $"{PluginName}.log");
-            }
+            LogFilePath = Path.Combine(folder, $"{PluginName}.log");
             OnMessageReceivedPatch.Initialize();
             Harmony.PatchAll();
             ServerEvents.RoundRestarted += OnRoundRestart;
         }
-
         public override void Disable()
         {
             Harmony.UnpatchAll();
---
+++ Patches/Scp914UpgraderPatch.cs
@@ -0,0 +1,91 @@
+﻿using HarmonyLib;
+using InventorySystem.Items.Armor;
+using InventorySystem.Items;
+using LabApi.Events.Arguments.Scp914Events;
+using LabApi.Events.Handlers;
+using PlayerRoles.FirstPersonControl;
+using Scp914;
+using Scp914.Processors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NorthwoodLib.Pools;
+using LabApi.Features.Console;
+using UnityEngine;
+using Logger = LabApi.Features.Console.Logger;
+
+namespace SCPSLBugPatch.Patches
+{
+    [HarmonyPatch(typeof(Scp914Upgrader), nameof(Scp914Upgrader.ProcessPlayer))]
+    internal class Scp914UpgraderPatch
+    {
+        private static bool Prefix(ReferenceHub ply, bool upgradeInventory, bool heldOnly, Scp914KnobSetting setting)
+        {
+            try
+            {
+                if (Physics.Linecast(ply.transform.position, Scp914Controller.Singleton.IntakeChamber.position, Scp914Upgrader.SolidObjectMask))
+                {
+                    return false;
+                }
+                Vector3 vector = ply.transform.position + Scp914Controller.MoveVector;
+                Scp914ProcessingPlayerEventArgs scp914ProcessingPlayerEventArgs = new Scp914ProcessingPlayerEventArgs(vector, setting, ply);
+                Scp914Events.OnProcessingPlayer(scp914ProcessingPlayerEventArgs);
+                if (!scp914ProcessingPlayerEventArgs.IsAllowed)
+                {
+                    return false;
+                }
+                setting = scp914ProcessingPlayerEventArgs.KnobSetting;
+                vector = scp914ProcessingPlayerEventArgs.NewPosition;
+                ply.TryOverridePosition(vector);
+                if (!upgradeInventory)
+                {
+                    return false;
+                }
+                HashSet<ushort> hashSet = HashSetPool<ushort>.Shared.Rent();
+                foreach (KeyValuePair<ushort, ItemBase> keyValuePair in ply.inventory.UserInventory.Items)
+                {
+                    if (!heldOnly || keyValuePair.Key == ply.inventory.CurItem.SerialNumber)
+                    {
+                        hashSet.Add(keyValuePair.Key);
+                    }
+                }
+                foreach (ushort key in hashSet)
+                {
+                    ItemBase itemBase;
+                    Scp914ItemProcessor scp914ItemProcessor;
+                    if (ply.inventory.UserInventory.Items.TryGetValue(key, out itemBase) && Scp914Upgrader.TryGetProcessor(itemBase.ItemTypeId, out scp914ItemProcessor))
+                    {
+                        ItemType itemTypeId = itemBase.ItemTypeId;
+                        Scp914ProcessingInventoryItemEventArgs scp914ProcessingInventoryItemEventArgs = new Scp914ProcessingInventoryItemEventArgs(itemBase, setting, ply);
+                        Scp914Events.OnProcessingInventoryItem(scp914ProcessingInventoryItemEventArgs);
+                        if (scp914ProcessingInventoryItemEventArgs.IsAllowed)
+                        {
+                            setting = scp914ProcessingInventoryItemEventArgs.KnobSetting;
+                            Scp914Upgrader.OnInventoryItemUpgraded?.Invoke(itemBase, setting);
+                            Scp914Result scp914Result = scp914ItemProcessor.UpgradeInventoryItem(setting, itemBase);
+                            ((Action<Scp914Result, Scp914KnobSetting>)typeof(Scp914Upgrader).Field(nameof(Scp914Upgrader.OnUpgraded)).GetValue(null))?.Invoke(scp914Result, setting);
+                            if (scp914Result.ResultingItems == null || !scp914Result.ResultingItems.TryGet(0, out ItemBase itemBase2))
+                            {
+                                itemBase2 = null;
+                            }
+                            if (itemBase2 != null)
+                            {
+                                Scp914Events.OnProcessedInventoryItem(new Scp914ProcessedInventoryItemEventArgs(itemTypeId, itemBase2, setting, ply));
+                            }
+                        }
+                    }
+                }
+                HashSetPool<ushort>.Shared.Return(hashSet);
+                BodyArmorUtils.SetPlayerDirty(ply);
+                Scp914Events.OnProcessedPlayer(new Scp914ProcessedPlayerEventArgs(vector, setting, ply));
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e);
+            }
+            return false;
+        }
+    }
+}
