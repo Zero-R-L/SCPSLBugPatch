75e32e72c9bc81f0b3cbcc06232623436ce891a6
ZeroRL <zerorl@qq.com>
2025-05-31 16:24:25 +0800

    Fixed

---
+++ Patches/LocalConnectionToServerDisconnenctPatch.cs
@@ -0,0 +1,23 @@
+﻿using Exiled.API.Features;
+using HarmonyLib;
+using Mirror;
+using System;
+using UnityEngine;
+using static PlayerList;
+
+namespace SCPSLBugPatch.Patches
+{
+    [HarmonyPatch(typeof(LocalConnectionToServer), nameof(LocalConnectionToServer.Disconnect))]
+    internal class LocalConnectionToServerDisconnenctPatch
+    {
+        public static bool Prefix(LocalConnectionToServer __instance)
+        {
+            if (__instance.identity.isServer)
+            {
+                MainClass.AddLog("[LocalConnectionToServer-DISCONNECT] Avoid a crash");
+                return false;
+            }
+            return true;
+        }
+    }
+}
