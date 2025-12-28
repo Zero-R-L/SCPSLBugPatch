2779cc0e5ac93362c3b0a92f677b4bb7957ac126
ZeroRL <zerorl@qq.com>
2025-12-28 15:47:10 +0800

    NewPatch

---
+++ Patches/LogSpamPatch.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using Mirror;
+using NetworkManagerUtils.Dummies;
+using PlayerRoles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCPSLBugPatch.Patches
+{
+    [HarmonyPatch(typeof(NetworkServer), nameof(NetworkServer.BroadcastToConnection))]
+    internal static class LogSpamPatch
+    {
+        private static bool Prefix(NetworkConnectionToClient connection)
+        {
+            foreach (NetworkIdentity item in connection.observing)
+            {
+                if (item != null)
+                {
+                    NetworkWriter networkWriter = NetworkServer.SerializeForConnection(item, connection);
+                    if (networkWriter != null)
+                    {
+                        EntityStateMessage entityStateMessage = default(EntityStateMessage);
+                        entityStateMessage.netId = item.netId;
+                        entityStateMessage.payload = networkWriter.ToArraySegment();
+                        EntityStateMessage message = entityStateMessage;
+                        connection.Send(message);
+                    }
+                }
+                else
+                {
+                    //Debug.LogWarning($"Found 'null' entry in observing list for connectionId={connection.connectionId}. Please call NetworkServer.Destroy to destroy networked objects. Don't use GameObject.Destroy.");
+                }
+            }
+            return false;
+        }
+    }
+}
